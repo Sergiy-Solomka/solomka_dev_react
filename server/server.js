3f1476846e93c12194289ee71c56abe168e311fa
Sergiy Solomka <ssolomka@gmail.com>
2023-04-30 10:13:23 +0200

    Added server part, now its can run easy.

---
+++ server/server.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const express = require("express");
+const {join} = require("path");
+require('dotenv').config()
+
+// Constants
+const PORT = process.env.PORT || 80;
+const HOST = process.env.HOST || "0.0.0.0";
+
+// App
+const app = express();
+app.use(express.static(__dirname + '/../frontend'));
+app.get('/', function(req, res) {
+  res.sendFile(join(__dirname, '/../frontend/index.html'));
+});
+
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
