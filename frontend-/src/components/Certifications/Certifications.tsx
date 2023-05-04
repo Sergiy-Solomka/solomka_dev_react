4a358aa156441d374fd2524bb020a5fd0f6395be
Sergiy Solomka <ssolomka@gmail.com>
2023-05-04 11:57:20 +0200

    Rebuild app, now server can read build folder.

--- frontend-/src/components/Certifications/Certifications.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Certifications = () => {
-  return <div></div>
-}
-
-export default Certifications
--- frontend-/src/components/ContactMe/ContactMe.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ContactMe = () => {
-  return <div></div>
-}
-
-export default ContactMe
